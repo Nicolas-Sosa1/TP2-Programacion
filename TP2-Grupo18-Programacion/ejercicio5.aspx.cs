76d51036889eaa3150de030ade0a80d58e1eaf0c
Alanjaqs <alannjaqs@gmail.com>
2025-04-13 22:58:07 -0300

    Carga de DropDownList ejercicio 5

--- TP2-Grupo18-Programacion/ejercicio5.aspx.cs
+++ TP2-Grupo18-Programacion/ejercicio5.aspx.cs
@@ -11,7 +11,13 @@ namespace TP2_Grupo18_Programacion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                //DDL
+                ddlMemoria.Items.Add(new ListItem("2GB", "200"));
+                ddlMemoria.Items.Add(new ListItem("4GB", "375"));
+                ddlMemoria.Items.Add(new ListItem("6GB", "500"));
+            }
         }
 
         protected void btnVolverPrincipal_Click(object sender, EventArgs e)
